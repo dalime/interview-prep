181f0a1d4be13ebff794874b69b06cce378a836a
duhdanube <Danny.B.Lim@gmail.com>
2019-04-13 14:28:05 +0800

    Find height of binary tree

---
+++ algorithms/trees/findHeightOfBinaryTree.java
@@ -0,0 +1,88 @@
+class BinaryTreeHeight {
+  class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int x) {
+      this.data = x;
+      this.left = null;
+      this.right = null;
+    }
+
+    public int getData() {
+      return this.data;
+    }
+
+    public Node getLeft() {
+      return this.left;
+    }
+
+    public Node getRight() {
+      return this.right;
+    }
+
+    public void setLeft(Node x) {
+      this.left = x;
+    }
+
+    public void setRight(Node x) {
+      this.right = x;
+    }
+  }
+
+  Node root;
+
+  public BinaryTreeHeight() {
+    this.root = null;
+  }
+
+  public Node insert(Node root, int data) {
+    if (root == null) {
+      root = new Node(data);
+    } else if (data <= root.getData()) {
+      root.setLeft(insert(root.getLeft(), data));
+    } else {
+      root.setRight(insert(root.getRight(), data));
+    }
+    return root;
+  }
+
+  public boolean search(Node root, int data) {
+    if (root == null) {
+      return false;
+    } else if (data == root.getData()) {
+      return true;
+    } else if (data <= root.getData()) {
+      return search(root.getLeft(), data);
+    } else {
+      return search(root.getRight(), data);
+    }
+  }
+
+  public int getHeight(Node root) {
+    if (root == null) {
+      return -1;
+    }
+    int leftHeight = getHeight(root.getLeft());
+    int rightHeight = getHeight(root.getRight());
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  public int findHeight() {
+    return getHeight(this.root);
+  }
+
+  public static void main(String[] args) {
+    BinaryTreeHeight myTree = new BinaryTreeHeight();
+    Node root = myTree.root;
+    root = myTree.insert(root, 8);
+    myTree.root = root;
+    root = myTree.insert(root, 10);
+    root = myTree.insert(root, 4);
+    root = myTree.insert(root, 2);
+    root = myTree.insert(root, 6);
+    root = myTree.insert(root, 12);
+    System.out.println("Height of the tree is: " + myTree.findHeight());
+  }
+}
---
+++ algorithms/trees/findHeightOfBinaryTree.js
@@ -0,0 +1,18 @@
+var BinaryTree = require('../../datastructures/binarysearchtree/binarySearchTreeWithNodes');
+
+function getHeight(root) {
+  if (!root) return -1;
+  return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+}
+
+console.log('Starting job...');
+var myTree = new BinaryTree();
+var root = myTree.root;
+root = myTree.insert(root, 8);
+myTree.root = root;
+root = myTree.insert(root, 10);
+root = myTree.insert(root, 4);
+root = myTree.insert(root, 2);
+root = myTree.insert(root, 6);
+root = myTree.insert(root, 12);
+console.log('Height of tree: ' + getHeight(myTree.root));
